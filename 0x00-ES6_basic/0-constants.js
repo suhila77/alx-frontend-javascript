94203fc693202e5a2ca7d476365c64353fa50495
suhila77 <suhilab9@gmail.com>
2024-10-04 09:19:12 +0000

    2-arrow

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+export function taskFirst() {
+	var task = "I Perfer const when I can.';
+	return task;
+}
+
+const getLast(is okay) {
+	return ' is okay';
+}
+
+const taskNext() {
+	var combiantion => 'But sometimes let';
+	combination += getLast();
+
+	return combinaton;
+}
